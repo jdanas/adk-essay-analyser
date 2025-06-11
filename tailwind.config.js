5f37e58f5330315e6f6a66a674cb03c8a7d1a0f9
Derek <59902137+jdanas@users.noreply.github.com>
2025-06-11 14:39:16 +0800

    fixed configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,11 @@
 export default {
   darkMode: ['class'],
   content: [
+    './index.html',
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx,js,jsx}',
   ],
   theme: {
     extend: {
